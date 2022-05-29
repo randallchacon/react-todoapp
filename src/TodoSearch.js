ba335977363e00fd25aa4cbf4e595508905ed0d7
randallchacon <info@randallchacon.com>
2022-05-28 23:09:11 -0600

    c8 button, todoitem and todosearch modified

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch(){
+const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+};
+
     return(
-        <input className='TodoSearch' placeholder="onion"/>
+        <input className='TodoSearch' placeholder="onion"
+        onChange={onSearchValueChange}
+        />
     );
 }
 
