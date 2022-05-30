5db060bedd04632479191ed52ae0d3eb4e51e0e2
randallchacon <info@randallchacon.com>
2022-05-29 19:04:46 -0600

    icon components added

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CompleteIcon } from '../TodoIcon/CompleteIcon';
+import { DeleteIcon } from '../TodoIcon/DeleteIcon';
 import './TodoItem.css';
 
 function TodoItem(props) {
@@ -10,18 +12,25 @@ function TodoItem(props) {
     }*/
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+{/*       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}
       >
         √
-      </span>
+      </span> */}
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete"
+{/*       <span className="Icon Icon-delete"
       onClick={props.onDelete}>
         X
-      </span>
+      </span> */}
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
     </li>
   );
 }
